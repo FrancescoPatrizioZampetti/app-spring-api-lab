e48ce7b8de259f9fec61dd45d0fa08f9012225a4
Francesco Patrizio Zampetti <francescopatrizio.zampetti@mcc.it>
2026-05-13 15:21:25 +0200

    first commit

---
+++ src/main/java/com/blackphoenixproductions/app_spring_api_lab/AppSpringApiLabApplication.java
@@ -0,0 +1,13 @@
+package com.blackphoenixproductions.app_spring_api_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringApiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringApiLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackphoenixproductions/app_spring_api_lab/AppSpringApiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackphoenixproductions.app_spring_api_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringApiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
