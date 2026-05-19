a0f9a66e818755484d96a07fcdfab5fbc192255a
Francesco Patrizio Zampetti <francescopatrizio.zampetti@mcc.it>
2026-05-19 22:37:20 +0200

    docker compose

--- docker-keycloak-dev.sh
+++ docker-keycloak-dev.sh
@@ -9,3 +9,6 @@
 #docker run --rm --name keycloak-lab -p 127.0.0.1:8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin -v ./docker/keycloak/import:/opt/keycloak/data/import:ro quay.io/keycloak/keycloak:26.6.2 start-dev --import-realm
 
 docker compose up
+
+# per esportare i dati del realm in un file json
+#docker run --rm -v keycloak-lab-data:/opt/keycloak/data -v %cd%/docker/keycloak/export:/opt/keycloak/export quay.io/keycloak/keycloak:26.6.2 export --dir /opt/keycloak/export --realm spring-api-lab --users realm_file
